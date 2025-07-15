de6d7e5d53af702a7835933c5d2692f10d7c7da7
Shruti Sharma <dcsa2325shrutisharma@kuk.ac.in>
2025-07-15 15:08:47 +0530

    404 Fix on mobile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,41 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingScreen from "./pages/LandingScreen";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import ContextProvider from './context/ContextProvider.jsx'
+import ContextProvider from './context/ContextProvider.jsx';
+import PrivateRoute from './components/PrivateRoute.jsx'
+import PublicRoute from './components/PublicRoutes.jsx';
+
 function App() {
-
-
   return (
     <BrowserRouter>
-    <ContextProvider>
-      <Routes>
-        <Route path="/" element={ <LandingScreen />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/register" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <ContextProvider>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={
+            <PublicRoute>
+              <LandingScreen />
+            </PublicRoute>
+          } />
+          <Route path="/register" element={
+            <PublicRoute>
+              <Signup />
+            </PublicRoute>
+          } />
+          <Route path="/login" element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          } />
+
+          {/* Private routes */}
+          <Route path="/home" element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          } />
+        </Routes>
       </ContextProvider>
     </BrowserRouter>
   );
